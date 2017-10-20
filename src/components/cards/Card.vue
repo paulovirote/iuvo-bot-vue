11b2d405289ccdb660efde8b1e02711c038f4629
Milene Vieira Lacerda <milenelacerda@icloud.com>
2017-10-20 14:32:43 -0200

    Criando componente cards

---
+++ src/components/cards/Card.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <div v-for="project in projects" :key="project.id" class="card">
+      <div class="card-image">
+        <figure class="image is-4by3">
+          <img :src="'./static/img/' + project.image" alt="Image">
+        </figure>
+      </div>
+      <div class="card-content">
+        <div class="media">
+          <div class="media-left">
+            <figure class="image is-48x48">
+              <img src="../../../static/img/bg-hello-page.jpg" alt="Image">
+            </figure>
+          </div>
+          <div class="media-content">
+            <p class="title is-4">{{ project.title }}</p>
+            <p class="subtitle is-6">@projeto</p>
+          </div>
+        </div>
+
+        <div class="content">
+
+          {{ project.description }}
+
+          <a>@projeto</a>.
+          <a>#categoria</a>
+          <a>#categoria</a>
+          <br>
+          <small>09:09 AM - 25 Agosto 2017</small>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import { bulmaComponentGenerator } from 'vue-bulma-components'
+import { listProjects } from '../../services/projects/ProjectService'
+
+export default {
+
+  data() {
+    return {
+      projects: []
+    }
+  },
+
+  components: {
+    box: bulmaComponentGenerator('box'),
+  },
+
+  methods: {
+    fetchProjects() {
+      listProjects()
+        .then(data => {
+          this.projects = data
+        })
+    }
+  },
+
+  mounted() {
+    this.fetchProjects()
+  },
+
+  computed: {
+    list() {
+      return this.projects
+    }
+  }
+
+}
+</script>
+<style>
+</style>
+
