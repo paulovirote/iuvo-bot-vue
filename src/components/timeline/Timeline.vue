629f1977225883456be43f39e0d369f0c9f693bd
Milene Vieira Lacerda <milenelacerda@icloud.com>
2017-10-20 13:32:46 -0200

    Separando componentes

--- src/components/timeline/Timeline.vue
+++ src/components/timeline/Timeline.vue
@@ -1,46 +1,11 @@
 <template>
   <div>
-    <vulma-navbar class="navbar" show-menu-button='true' @menu-clicked='$refs.drawer.open()'>
-      <vulma-navitem slot='left' :is-tab='false'>
-        <img class="logo" src="../../../static/img/care.png">
-      </vulma-navitem>
-      <vulma-navitem class="title" slot='center'>
-        <h3 class="page-title">{{ title }}</h3>
-      </vulma-navitem>
-    </vulma-navbar>
-    <vulma-drawer ref='drawer'>
-      <vulma-drawerheader name='Milene ' email='milenelacerda@icloud.com ' avatar='' color='custom-hulk '></vulma-drawerheader>
-      <vulma-menu>
-
-        <vulma-menuitem icon='fa-user'>{{ profileTile }}</vulma-menuitem>
-        <vulma-menugroup :title="configTitle">
-          <vulma-menuitem>{{ accountTitle }}</vulma-menuitem>
-          <vulma-menuitem>{{ paymentTitle }}</vulma-menuitem>
-          <vulma-menuitem>{{ projectsTitle }}</vulma-menuitem>
-        </vulma-menugroup>
-
-      </vulma-menu>
-    </vulma-drawer>
+    <menu-component></menu-component>
 
     <div class="wrapper">
-      
-      <div class="tabs is-centered is-boxed">
-        <ul>
-          <li class="is-active">
-            <a>
-              <span>{{ filters.all }}</span>
-            </a>
-          </li>
-          <li>
-            <a>
-              <span>{{ filters.animals }}</span>
-            </a>
-          </li>
-        </ul>
-      </div>
-      
-
-      <div class="wrapper-card">
+      <tabs></tabs>
+    
+      <!-- <div class="wrapper-card">
         <div v-for="project in projects" :key="project.id" class="card">
           <div class="card-image">
             <figure class="image is-4by3">
@@ -72,34 +37,28 @@
             </div>
           </div>
         </div>
-      </div>
+      </div> -->
     </div>
   </div>
 </template>
 <script>
 import { bulmaComponentGenerator } from 'vue-bulma-components'
+import tabsComponent from '../tabs/Tabs'
+import menuComponent from '../menu/Menu'
 import { listProjects } from '../../services/projects/ProjectService'
 
 export default {
   name: 'timeline',
 
   components: {
-    box: bulmaComponentGenerator('box')
+    box: bulmaComponentGenerator('box'),
+    'tabs': tabsComponent,
+    'menu-component': menuComponent
   },
 
   data () {
     return {
-      projects: [],
-      title: 'Projetos',
-      profileTile: 'Perfil',
-      configTitle: 'Configurações',
-      accountTitle: 'Conta',
-      paymentTitle: 'Pagamento',
-      projectsTitle: 'Projetos',
-      filters: {
-        all: 'all',
-        animals: 'animals'
-      }
+      projects: []
     }
   },
 
@@ -125,32 +84,6 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-.bgcolor-custom-hulk 
-  color: white
-  background-color: #00d1b2
-  
-.navbar
-  background-color: #00d1b2 !important
-  min-height: 4.25rem !important
-
-.wrapper
-  margin: 0 auto
-  padding: 90px 20px 20px
-
-.wrapper-card
-  max-width: 450px
-  padding-top: 20px
-  margin: 0 auto 10px
-  
-.menu-button[data-v-9d073fb2]
-  transition: .4s ease
-
-.page-title 
-  font-size: 30px
-  color: #fff
-  cursor: default
-  font-family: 'Pacifico', cursive
-
 .card 
   margin-bottom: 40px;  
 </style>
